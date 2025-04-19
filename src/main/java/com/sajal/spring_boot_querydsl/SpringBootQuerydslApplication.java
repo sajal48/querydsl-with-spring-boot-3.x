5508967b3e4909be5ce10aeccb61687d212390af
Sajal <newtonh48@gmail.com>
2025-04-19 14:50:01 +0600

    git init

---
+++ src/main/java/com/sajal/spring_boot_querydsl/SpringBootQuerydslApplication.java
@@ -0,0 +1,13 @@
+package com.sajal.spring_boot_querydsl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootQuerydslApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootQuerydslApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sajal/spring_boot_querydsl/SpringBootQuerydslApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sajal.spring_boot_querydsl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootQuerydslApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
